fa5412a15ec8569df5c2012c3f16174c88d206eb
Rohit-Bhardwaj16 <rb8329563@gmail.com>
2024-09-23 00:47:22 +0530

    before fixing clear everything

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
+"use strict";
 // dashboard elements
 let balnace_num = document.querySelector(".balance-number");
 let expense_num = document.querySelector(".expense-number");
 let income_num = document.querySelector(".income-number");
+
 //income section elements
+const income_amount_value = parseInt(document.querySelector(".amount").value);
 const income_amount = document.querySelector(".amount");
-// console.log(income_amount);
 const income_date = document.querySelector(".date");
 const income_remarks = document.querySelector(".remarks");
 const income_source = document.querySelector(".source");
 const income_button = document.querySelector(".add-inc");
 const income_history_ul = document.querySelector(".listTrans");
-// const income_chart=;
 
 //budget form elements
 const budget_amount = document.querySelector(".budget");
 const budget_type = document.querySelector(".type");
 const budget_button = document.querySelector("#next-btn");
 const budget_list_ul = document.querySelector("#budget-list-ul");
+
 //expense section details
 const expense_amount = document.querySelector("#expense-amount");
 const expense_date = document.querySelector("#expense-date");
@@ -25,250 +27,173 @@ const expense_source = document.querySelector("#expense-source");
 const expense_button = document.querySelector("#expense-btn");
 const expense_history_ul = document.querySelector("#expenseChk");
 
-//peventing the default behaviour of forms
 const income_Form = document.querySelector(".income-form");
 const expense_form = document.querySelector(".expense-form");
 
-income_button.addEventListener("click", function (e) {
-  e.preventDefault();
-});
-expense_button.addEventListener("click", function (e) {
-  e.preventDefault();
-});
-let temp_amount = 0;
-//function to update income,history and expense history...
-function add(amount, date, remarks, source, ul, num) {
+function preventsubmission(event) {
+  event.preventDefault();
+}
+
+function cleareverything(amount, date, remarks, source) {
+    amount.value = "";
+    date.value = "";
+    remarks.innerText = "";
+    source.value = "select type";
+  }
+ 
+//peventing the default behaviour of forms
+income_button.addEventListener("click", preventsubmission);
+expense_button.addEventListener("click", preventsubmission);
+budget_button.addEventListener("click", preventsubmission);
+
+
+const income_array = [];
+const expense_array = [];
+const balance_array = [];
+
+//craeting history list element
+function createlistelement(amount, remarks, date, source, ul) {
+  let li = document.createElement("li");
+
+  li.innerHTML = `<span class = "color">${amount.value}</span> from <span class="color">${remarks.value}</span> on <span class="color">${date.value}</span> as <span class="color" >${source.value}</span> `;
+
+  ul.appendChild(li);
+}
+
+
+function addhistory(amount,date,remarks,source,ul){
   if (
     amount.value === "" ||
     date.value === "" ||
     remarks.value === "" ||
-    source.value === ""
+    source.value === "select type"
   ) {
     alert("please fill out the neccesary feilds!");
-  } else if (amount.value > 0 && source.value != "select type") {
-    let li = document.createElement("li");
-
-    li.innerHTML = `<span class = "color">${amount.value}</span> from <span class="color">${remarks.value}</span> on <span class="color">${date.value}</span> as <span class="color" >${source.value}</span> `;
-
-    ul.appendChild(li);
-
-    let current = parseInt(num.value) || 0;
-    let newcr = parseInt(amount.value) || 0;
-    num.value = current + newcr;
-    let update = num.value;
-    num.innerHTML = update;
+  } else if (amount.value < 0) {
+    alert("BUG dhundh rhe ho kya,nii milega..");
+    cleareverything(amount, date, remarks, source);
   } else {
-    alert("input correct data");
+    createlistelement(
+      amount,
+      remarks,
+      date,
+      source,
+      ul
+    );
+    // cleareverything(amount, date, remarks, source);
   }
-  temp_amount = amount.value;
-  amount.value = "";
-  date.value = "";
-  remarks.value = "";
-  source.value = "select type";
 }
 
-document.addEventListener("DOMContentLoaded", initializeCharts);
-let incomeChart, expenseChart;
-
-function initializeCharts() {
-  const ctxIncome = document.getElementById("incomeChart").getContext("2d");
-  incomeChart = new Chart(ctxIncome, {
-    type: "pie",
-    data: {
-      labels: [], // dates or remarks
-      datasets: [
-        {
-          label: "income",
-          data: [],
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(255, 159, 64, 0.2)",
-          ],
-          borderColor: [
-            "rgba(255, 99, 132, 1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)",
-            "rgba(75, 192, 192, 1)",
-            "rgba(153, 102, 255, 1)",
-            "rgba(255, 159, 64, 1)",
-          ],
-          borderWidth: 1,
-        },
-      ],
-    },
-    options: {
-      responsive: true,
-      plugins: {
-        legend: {
-          position: "top",
-        },
-        title: {
-          display: true,
-          text: "income chart",
-        },
-      },
-    },
-  });
-
-  const ctxExpense = document.getElementById("incomeChart").getContext("2d");
-  expenseChartChart = new Chart(ctxExpense, {
-    type: "pie",
-    data: {
-      labels: [], // dates or remarks
-      datasets: [
-        {
-          label: "expense",
-          data: [],
-          backgroundColor: [
-            "rgba(255, 99, 132, 0.2)",
-            "rgba(54, 162, 235, 0.2)",
-            "rgba(255, 206, 86, 0.2)",
-            "rgba(75, 192, 192, 0.2)",
-            "rgba(153, 102, 255, 0.2)",
-            "rgba(255, 159, 64, 0.2)",
-          ],
-          borderColor: [
-            "rgba(255, 99, 132, 1)",
-            "rgba(54, 162, 235, 1)",
-            "rgba(255, 206, 86, 1)",
-            "rgba(75, 192, 192, 1)",
-            "rgba(153, 102, 255, 1)",
-            "rgba(255, 159, 64, 1)",
-          ],
-          borderWidth: 1,
-        },
-      ],
-    },
-    options: {
-      responsive: true,
-      plugins: {
-        legend: {
-          position: "top",
-        },
-        title: {
-          display: true,
-          text: "expense chart",
-        },
-      },
-    },
-  });
+function updatedashboard(array,num) {
+  const total = array.reduce((acc, income) => acc + income, 0);
+  if(total>0){
+    num.innerHTML = total;
+  }
 }
 
-function updateChart(chart, label, amount) {
-  chart.data.labels.push(label);
-  chart.data.datasets[0].data.push(amount);
-  chart.update();
+function update_balance(){
+  const inc=parseInt(income_amount.value) || 0
+  const exp=parseInt(expense_amount.value) || 0
+  console.log(expense_amount);
+  let balance=inc-exp
+  console.log(balance);
+  console.log(typeof balance);
+  balnace_num.innerHTML=balance
 }
 
-balnace_num.innerHTML = 0;
-income_num.innerHTML = 0;
-income_button.addEventListener("click", function () {
-  add(
-    income_amount,
-    income_date,
-    income_remarks,
-    income_source,
-    income_history_ul,
-    income_num
-  );
-  //updating balance_num
-  balnace_num.value = income_num.value || 0;
-  balnace_num.textContent = balnace_num.value || 0;
-  updateChart(incomeChart, income_date.value, income_amount.value);
-});
+// setting the canvas for the charts...
+const inc_canvas=document.querySelector("#incomeChart")
+const exp_canvas=document.querySelector("#expenseChart")
 
-let my_expense;
-let my_budget;
-let result;
+inc_canvas.width="450"
+inc_canvas.height="550"
+exp_canvas.width="450"
+exp_canvas.height="550"
 
-budget_button.addEventListener("click", function (e) {
-  e.preventDefault();
-});
+const budget_array=[]
 
-let temp_budget=0;
-let temp_budget_type;
-let temp_expense_source;
-budget_button.addEventListener("click", function () {
-  if (budget_amount.value === "" || budget_type.value === "") {
-    alert("fill out the neccesary details!");
-  } else if (budget_amount.value > 0) {
+//updating income
+income_button.addEventListener("click", handleincome);
+function handleincome() {
+  income_array.push(parseInt(income_amount.value));
+  addhistory(income_amount,income_date,income_remarks,income_source,income_history_ul);
+  updatedashboard(income_array,income_num);
+}
+
+// managing budget options
+budget_button.addEventListener('click',handlebudget)
+
+let temp_budget;
+function handlebudget(){ 
+  budget_array.push(parseInt(budget_amount.value))
+  temp_budget=budget_amount.value
+  makebudgetbar()
+}
+
+function bar_length() {
+  let bar_length = ((expense_amount.value / temp_budget) * 100);
+  return bar_length;
+}
+
+function makebudgetbar(){
+  if (budget_amount.value === "" || budget_type.value === "" || budget_amount<0) {
+    alert("fill out the neccesary details correctly!");
+  } else{
     let li = document.createElement("li");
-
     li.innerHTML = `
                         <p class="inLi">${budget_type.value}</p>
                         <span style="display:inline">${budget_amount.value}</span>
                         <div class="bar">
-                            <div class="bar-color" style="width:0%"></div>
+                            <div class="bar-color" style="width:${bar_length()}%"></div>
                         </div>
                         <span class="progress" style="dispaly:inline;"></span>
                     `;
     budget_list_ul.appendChild(li);
-
     let option = document.createElement("option");
     option.innerHTML = ` <option value="${budget_type.value}">${budget_type.value}</option>`;
     document.querySelector("#expense-source").appendChild(option);
-    temp_budget = budget_amount.value;
-    temp_budget_type = budget_type.value;
-  } else {
-    alert("input correct data");
-  }
+    // cleareverything(budget_amount,budget_type)
+}
+}
 
-  budget_amount.value = "";
-  budget_type.value = "";
-});
-let temp_expense=0;
-let initial = 0;
-expense_num.innerHTML = 0;
-expense_button.addEventListener("click", function () {
-  temp_expense = expense_amount.value;
-  temp_expense_source = expense_source.value;
-  add(
-    expense_amount,
-    expense_date,
-    expense_remarks,
-    expense_source,
-    expense_history_ul,
-    expense_num
-  );
+//managing th expense section
+
+expense_button.addEventListener('click',handleexpense)
 
 
-  //updating balance_num
-  let oldbalance = balnace_num.value || 0;
-  let newbalance = expense_num.value || 0;
-  balnace_num.value = oldbalance - newbalance;
-  balnace_num.textContent = balnace_num.value;
-  console.log(temp_expense);
+let length=0
+function handleexpense() {
+  expense_array.push(parseInt(expense_amount.value));
+  console.log(parseInt(expense_amount.value))
+  length+=bar_length()
+  addhistory(expense_amount,expense_date,expense_remarks,expense_source,expense_history_ul);
+  updatedashboard(expense_array,expense_num);
+  update_balance()
   console.log(temp_budget);
-  let budgets = document.querySelectorAll("#budget-list-ul li");
-  console.log(budgets);
-  budgets.forEach((budget) => {
-    let budgetType = budget.querySelector(".inLi").textContent;
-    console.log(budgetType);
-    console.log(temp_expense_source);
-    if (budgetType === temp_expense_source && temp_expense<temp_budget) {
-      let budgetValue = parseInt(budget.querySelector("span").textContent);
-      console.log(budgetValue);
-      let expenseValue = parseInt(expense_amount.value);
-      console.log(expenseValue);
-      let progressBar = budget.querySelector(".bar-color");
-      let progressText = budget.querySelector(".progress");
-      let currentProgress = parseFloat(progressText.textContent) || 0;
-      console.log(currentProgress);
-      let newProgress = currentProgress + (temp_amount / budgetValue) * 100;
-      console.log(newProgress);
-      progressBar.style.width = `${newProgress}%`;
-      progressText.textContent = `${newProgress.toFixed(2)}%`;
+  console.log(length);
+  console.log(budget_type.value);
+  console.log(expense_source.value);
+  updateBudgetBar(length)
+  // cleareverything(expense_amount,expense_date,expense_remarks,expense_source)
+}
+function updateBudgetBar(length) {
+  const bars= document.querySelectorAll("#budget-list-ul li");
+  console.log(budget_type.value); 
+  console.log(expense_source.value);
+  bars.forEach((bar)=>{
+    if(budget_type.value==expense_source.value){
+      let barnew= bar.querySelector(".bar-color");
+        barnew.style.width = `${length}%`;
+        console.log(length)
+        console.log("if working")
     }
     else{
       alert("budget limit exceeded")
     }
-  });
-// }
-// else{
-//   alert("budget exceede!")
-// }
-});
+  })
+} 
+
+//selecting the latest element of expense history (by adding the class number  to it and increasing that number by 1 each time)
+
+
+
